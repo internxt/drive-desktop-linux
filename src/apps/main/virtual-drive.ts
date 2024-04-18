613ce776309af71c93d147a55669676fe282c4a3
joan vicens <joanvicens@proton.me>
2024-04-18 12:20:47 +0200

    install and unsintall on login events

--- src/apps/main/virtual-drive.ts
+++ src/apps/main/virtual-drive.ts
@@ -1,16 +1,3 @@
-import { installNautilusExtension } from '../nautilus-extension/install';
-import Logger from 'electron-log';
-import * as Sentry from '@sentry/electron/main';
-
 if (process.platform === 'linux') {
   import('../fuse/index');
 }
-
-installNautilusExtension()
-  .then(() => {
-    Logger.info('[NAUTILUS EXTENSION] Extension Installed');
-  })
-  .catch((err) => {
-    Logger.error(err);
-    Sentry.captureException(err);
-  });
