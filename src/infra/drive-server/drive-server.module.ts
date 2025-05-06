965ad2901ca901230bdc407c55afc0b5844126ca
AlexisMora <alexis@internxt.com>
2025-05-06 12:29:04 +0200

    chore: added FilesService to DriveServerModule

--- src/infra/drive-server/drive-server.module.ts
+++ src/infra/drive-server/drive-server.module.ts
@@ -1,11 +1,13 @@
 import { AuthService } from './services/auth/auth.service';
 import { BackupService } from './services/backup/backup.service';
+import { FilesService } from './services/files/files.service';
 
 export class DriveServerModule {
 
   constructor(
     public auth = new AuthService(),
     public backup = new BackupService(),
+    public files = new FilesService(),
   ) {}
 }
 
