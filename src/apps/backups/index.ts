477ede771174732a7a55ac888b41adcb6b970c90
AlexisMora <alexis@internxt.com>
2026-01-08 14:41:03 +0100

    chore: Removed backupFolder since its just a caller for runWithRetry, logic will be used in launchBackupProcesses

--- src/apps/backups/index.ts
+++ src/apps/backups/index.ts
@@ -1,26 +1,6 @@
 import { ipcRenderer } from 'electron';
-import { BackupService } from './BackupService';
-import { BackupInfo } from './BackupInfo';
 import { BackupsDependencyContainerFactory } from './dependency-injection/BackupsDependencyContainerFactory';
 import { logger } from '@internxt/drive-desktop-core/build/backend';
-import { stopController } from '../../backend/features/backup';
-import { Either, left, right } from '../../context/shared/domain/Either';
-import { RetryError } from '../shared/retry/RetryError';
-
-export async function backupFolder(backupInfo: BackupInfo): Promise<Either<RetryError, void>> {
-  const container = await BackupsDependencyContainerFactory.build();
-  const backupService = container.get(BackupService);
-
-  const result = await backupService.runWithRetry(backupInfo, stopController);
-  if (result.isLeft()) {
-    const error = result.getLeft();
-    logger.debug({ tag: 'BACKUPS', msg: 'failed', error: error.cause });
-    return left(error);
-  } else {
-    logger.debug({ tag: 'BACKUPS', msg: 'Backup completed successfully' });
-    return right(undefined);
-  }
-}
 
 async function reinitializeBackups() {
   await BackupsDependencyContainerFactory.reinitialize();
