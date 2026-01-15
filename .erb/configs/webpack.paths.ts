2795a9264f5b9da92a257d5a1a4560f8c3384134
AlexisMora <alexis@internxt.com>
2026-01-15 14:02:09 +0100

    chore:reference new, unified package.json

--- .erb/configs/webpack.paths.ts
+++ .erb/configs/webpack.paths.ts
@@ -12,13 +12,8 @@ const srcBackupsPath = path.join(srcPath, 'backups');
 const srcSyncEnginePath = path.join(srcPath, 'sync-engine');
 const srcVirtualDrivePath = path.join(srcPath, 'drive');
 
-const releasePath = path.join(rootPath, 'release');
-const appPath = path.join(releasePath, 'app');
-const appPackagePath = path.join(appPath, 'package.json');
-const appNodeModulesPath = path.join(appPath, 'node_modules');
-const srcNodeModulesPath = path.join(srcPath, 'node_modules');
-
-const distPath = path.join(appPath, 'dist');
+// Flattened structure - dist and build at root level
+const distPath = path.join(rootPath, 'dist');
 const distMainPath = path.join(distPath, 'main');
 const distRendererPath = path.join(distPath, 'renderer');
 const distSyncPath = path.join(distPath, 'sync');
@@ -26,7 +21,7 @@ const distBackupsPath = path.join(distPath, 'backups');
 const distSyncEnginePath = path.join(distPath, 'sync-engine');
 const distVirtualDrivePath = path.join(distPath, 'drive');
 
-const buildPath = path.join(releasePath, 'build');
+const buildPath = path.join(rootPath, 'build');
 
 export default {
   rootPath,
@@ -34,11 +29,6 @@ export default {
   srcPath,
   srcMainPath,
   srcRendererPath,
-  releasePath,
-  appPath,
-  appPackagePath,
-  appNodeModulesPath,
-  srcNodeModulesPath,
   distPath,
   distMainPath,
   distRendererPath,
