ae1c0dcb2e63197b87dc1affc59d5f77f23a9888
AlexisMora <alexis@internxt.com>
2026-02-11 15:03:35 +0100

    feat: moveFile now uses new client

--- src/infra/drive-server/services/files/services/move-file.ts
+++ src/infra/drive-server/services/files/services/move-file.ts
@@ -1,10 +1,8 @@
 import { logger } from '@internxt/drive-desktop-core/build/backend/core/logger/logger';
 import { Result } from '../../../../../context/shared/domain/Result';
-import fetch from 'electron-fetch';
-import { FileError } from '../file.error';
-import { errorHandler } from './file-error-handler';
-import { mapError } from '../../utils/mapError';
 import { getNewApiHeaders } from '../../../../../apps/main/auth/service';
+import { driveServerClient } from '../../../client/drive-server.client.instance';
+import { DriveServerError } from '../../../drive-server.error';
 
 export async function moveFile({
   destinationFolder,
@@ -12,29 +10,23 @@ export async function moveFile({
 }: {
   destinationFolder: string;
   uuid: string;
-}): Promise<Result<boolean, FileError>> {
-  try {
-    const headers = getNewApiHeaders();
+}): Promise<Result<boolean, DriveServerError>> {
 
-    const response = await fetch(`${process.env.NEW_DRIVE_URL}/files/${uuid}`, {
-      method: 'PATCH',
-      headers,
-      body: JSON.stringify({
-        destinationFolder,
-      }),
-    });
-    if (response.ok) {
-      return { data: true };
-    }
-    return errorHandler(response);
-  } catch (error) {
-    const mappedError = mapError(error);
+  const { error } = await driveServerClient.PATCH('/files/{uuid}', {
+    path: { uuid },
+    body: {
+      destinationFolder,
+    },
+    headers: getNewApiHeaders(),
+  });
+
+  if (error) {
     logger.error({
       msg: 'Error moving file',
-      error: mappedError.message,
+      error,
+      path: `/files/${uuid}`,
     });
-    return {
-      error: new FileError('UNKNOWN'),
-    };
+    return { error };
   }
+  return { data: true };
 }
