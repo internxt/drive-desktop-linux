978b4ca10c5a55aae070e7e86df5cdf7bf1c7595
AlexisMora <alexis@internxt.com>
2026-02-16 15:48:53 +0100

    fix:format

--- src/apps/renderer/pages/Settings/Antivirus/views/LockedState.test.tsx
+++ src/apps/renderer/pages/Settings/Antivirus/views/LockedState.test.tsx
@@ -1,7 +1,6 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import { LockedState } from './LockedState';
 
-
 describe('LockedState', () => {
   beforeEach(() => {
     vi.clearAllMocks();
