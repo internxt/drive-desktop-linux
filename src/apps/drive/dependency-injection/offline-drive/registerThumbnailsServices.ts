15138270a161fa1ef958e8794c0b05dc4f72c602
Alexis Mora <alexis@internxt.com>
2026-02-26 09:49:05 +0100

    fix: remove broken endpoint for thumbnail retrieval (#258)

--- src/apps/drive/dependency-injection/offline-drive/registerThumbnailsServices.ts
+++ src/apps/drive/dependency-injection/offline-drive/registerThumbnailsServices.ts
@@ -1,34 +1,15 @@
-import { Environment } from '@internxt/inxt-js';
 import { ContainerBuilder } from 'diod';
-import os from 'os';
-import path from 'path';
+import os from 'node:os';
+import path from 'node:path';
 import { ThumbnailSynchronizer } from '../../../../context/storage/thumbnails/application/sync/ThumbnailSynchronizer';
 import { LocalThumbnailRepository } from '../../../../context/storage/thumbnails/infrastructrue/local/LocalThumbnsailsRepository';
 import { SystemThumbnailNameCalculator } from '../../../../context/storage/thumbnails/infrastructrue/local/SystemThumbnailNameCalculator';
-import { EnvironmentThumbnailDownloader } from '../../../../context/storage/thumbnails/infrastructrue/remote/EnvironmentThumbnailDownloader';
-import { RemoteThumbnailsRepository } from '../../../../context/storage/thumbnails/infrastructrue/remote/RemoteThumbnailsRepository';
 import { RelativePathToAbsoluteConverter } from '../../../../context/virtual-drive/shared/application/RelativePathToAbsoluteConverter';
-import { AuthorizedClients } from '../../../shared/HttpClient/Clients';
-import { DependencyInjectionUserProvider } from '../../../shared/dependency-injection/DependencyInjectionUserProvider';
 
 export async function registerThumbnailsServices(builder: ContainerBuilder) {
-  const user = DependencyInjectionUserProvider.get();
-
-  builder.register(EnvironmentThumbnailDownloader).useFactory((c) => {
-    return new EnvironmentThumbnailDownloader(c.get(Environment).download, user.bucket);
-  });
-
   builder.registerAndUse(SystemThumbnailNameCalculator);
 
   builder.register(ThumbnailSynchronizer).useFactory((c) => {
-    const clients = c.get(AuthorizedClients);
-
-    const remote = new RemoteThumbnailsRepository(
-      //@ts-ignore
-      clients.newDrive,
-      c.get(EnvironmentThumbnailDownloader),
-    );
-
     const pathConverter = c.get(RelativePathToAbsoluteConverter);
 
     const local = new LocalThumbnailRepository(
@@ -39,16 +20,6 @@ export async function registerThumbnailsServices(builder: ContainerBuilder) {
 
     local.init();
 
-    return new ThumbnailSynchronizer(remote, local);
+    return new ThumbnailSynchronizer(local);
   });
-
-  // const appData = app.getPath('appData');
-  // const local = path.join(appData, 'internxt-drive', 'thumbnails');
-
-  // const systemDefaultIconsGenerator = new SystemDefaultIconsGenerator(local);
-  // await systemDefaultIconsGenerator.init();
-
-  // builder
-  //   .register(SystemDefaultIconsGenerator)
-  //   .useInstance(systemDefaultIconsGenerator);
 }
