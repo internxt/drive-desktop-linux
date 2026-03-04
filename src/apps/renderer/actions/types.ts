40a2a6529c5e72f5c0f2df6ac84e2d0221104611
Esteban Galvis <egalvis@shokworks.io>
2026-03-04 16:49:11 -0500

    feat: remove dead dependencies and update package versions (#256)

--- src/apps/renderer/actions/types.ts
+++
@@ -1,8 +0,0 @@
-import { FatalError } from '../../../shared/issues/FatalError';
-
-export interface Action {
-  name: string;
-  func: (() => void) | ((error: undefined) => Promise<void>);
-}
-
-export type FatalErrorActionMap = Record<FatalError, Action>;
