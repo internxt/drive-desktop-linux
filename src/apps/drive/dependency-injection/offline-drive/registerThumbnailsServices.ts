9b06af790af63a32a9e48d167b4cb174461078c0
AlexisMora <alexis@internxt.com>
2026-03-05 16:00:05 +0100

    chore: centralize thumbnails path on core folder

--- src/apps/drive/dependency-injection/offline-drive/registerThumbnailsServices.ts
+++ src/apps/drive/dependency-injection/offline-drive/registerThumbnailsServices.ts
@@ -1,10 +1,9 @@
 import { ContainerBuilder } from 'diod';
-import os from 'node:os';
-import path from 'node:path';
 import { ThumbnailSynchronizer } from '../../../../context/storage/thumbnails/application/sync/ThumbnailSynchronizer';
 import { LocalThumbnailRepository } from '../../../../context/storage/thumbnails/infrastructrue/local/LocalThumbnsailsRepository';
 import { SystemThumbnailNameCalculator } from '../../../../context/storage/thumbnails/infrastructrue/local/SystemThumbnailNameCalculator';
 import { RelativePathToAbsoluteConverter } from '../../../../context/virtual-drive/shared/application/RelativePathToAbsoluteConverter';
+import { PATHS } from '../../../../core/electron/paths';
 
 export async function registerThumbnailsServices(builder: ContainerBuilder) {
   builder.registerAndUse(SystemThumbnailNameCalculator);
@@ -15,7 +14,7 @@ export async function registerThumbnailsServices(builder: ContainerBuilder) {
     const local = new LocalThumbnailRepository(
       pathConverter,
       c.get(SystemThumbnailNameCalculator),
-      path.join(os.homedir(), '.cache', 'thumbnails'),
+      PATHS.THUMBNAILS_FOLDER,
     );
 
     local.init();
