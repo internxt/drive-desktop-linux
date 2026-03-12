f81dadb22e6b296cc49c1cdce73c1ba994f70e21
AlexisMora <alexis@internxt.com>
2026-03-12 12:02:31 +0100

    chore: code cleanup

--- src/apps/drive/fuse/FuseApp.test.ts
+++ src/apps/drive/fuse/FuseApp.test.ts
@@ -24,20 +24,13 @@ import { FileRepositorySynchronizer } from '../../../context/virtual-drive/files
 import { logger } from '@internxt/drive-desktop-core/build/backend';
 import { Container } from 'diod';
 import { VirtualDrive } from '../virtual-drive/VirtualDrive';
-import configStore from '../../main/config';
-import { getExistingFiles } from '../../main/remote-sync/service';
-import { Mock } from 'vitest';
 
 describe('FuseApp', () => {
   let fuseApp: FuseApp;
   let fileRepositoryMock: FileRepositorySynchronizer;
-  const startDate = new Date('2025-02-19T12:00:00Z');
-  const endDate = new Date('2025-03-04T15:30:00Z');
 
   beforeEach(() => {
-    fileRepositoryMock = {
-      fixDanglingFiles: vi.fn(),
-    } as unknown as FileRepositorySynchronizer;
+    fileRepositoryMock = {} as unknown as FileRepositorySynchronizer;
 
     const containerMock = {
       get: vi.fn((service) => {
@@ -58,55 +51,5 @@ describe('FuseApp', () => {
 
   afterEach(() => {
     vi.clearAllMocks();
-  });
-
-  describe('fixDanglingFiles', () => {
-    it('should not continue with the execution if configStore.shouldFixDanglingFiles returns false', async () => {
-      (configStore.get as Mock).mockReturnValue(false);
-      await fuseApp.fixDanglingFiles(startDate, endDate);
-
-      expect(getExistingFiles).not.toHaveBeenCalled();
-      expect(fileRepositoryMock.fixDanglingFiles).not.toHaveBeenCalled();
-    });
-
-    it('should fix only the files that are within a time period', async () => {
-      (configStore.get as Mock).mockReturnValue(true);
-      const mockFiles = [
-        { fileId: 'file1', createdAt: '2025-02-20T10:00:00Z' }, // Within range
-        { fileId: 'file2', createdAt: '2025-03-05T10:00:00Z' }, // Out of range
-      ];
-
-      (getExistingFiles as Mock).mockResolvedValue(mockFiles);
-      (fileRepositoryMock.fixDanglingFiles as Mock).mockResolvedValue(true);
-
-      await fuseApp.fixDanglingFiles(startDate, endDate);
-
-      expect(fileRepositoryMock.fixDanglingFiles).toHaveBeenCalledWith(['file1']);
-    });
-
-    it('should not fix any files if there arent any', async () => {
-      (configStore.get as Mock).mockReturnValue(true);
-      const mockFiles = [
-        { fileId: 'file1', createdAt: '2025-03-10T10:00:00Z' }, // Out of range
-      ];
-      (getExistingFiles as Mock).mockResolvedValue(mockFiles);
-
-      await fuseApp.fixDanglingFiles(startDate, endDate);
-
-      expect(fileRepositoryMock.fixDanglingFiles).not.toHaveBeenCalled();
-    });
-
-    it('should set the configStore.shouldFixDanglingFiles to false if all the files are fixed', async () => {
-      (configStore.get as Mock).mockReturnValue(true);
-      const mockFiles = [
-        { fileId: 'file1', createdAt: '2025-02-20T10:00:00Z' }, // Within range
-      ];
-      (getExistingFiles as Mock).mockResolvedValue(mockFiles);
-      (fileRepositoryMock.fixDanglingFiles as Mock).mockResolvedValue(true);
-
-      await fuseApp.fixDanglingFiles(startDate, endDate);
-
-      expect(configStore.set).toHaveBeenCalledWith('shouldFixDanglingFiles', false);
-    });
   });
 });
