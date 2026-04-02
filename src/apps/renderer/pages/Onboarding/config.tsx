dfb5a56332d3081144e09a7cbc2e7dbad6aef565
Esteban Galvis <egalvis@shokworks.io>
2026-04-02 14:32:21 -0500

    fix: correct positioning of onboarding completed image

--- src/apps/renderer/pages/Onboarding/config.tsx
+++ src/apps/renderer/pages/Onboarding/config.tsx
@@ -246,7 +246,7 @@ export const SLIDES: OnboardingSlide[] = [
       const { theme } = useTheme();
       return (
         <div className="relative h-full w-full overflow-hidden">
-          <div className="absolute top-[80px] left-[76px]">
+          <div className="absolute left-[76px] top-[80px]">
             <SideImageAnimation display>{getLinuxFileExplorerImage(theme)}</SideImageAnimation>
           </div>
         </div>
