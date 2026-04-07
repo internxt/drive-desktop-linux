890d323045f2edd7e2113911a96aefc118c54d44
Alexis Mora <alexis@internxt.com>
2026-04-07 15:38:02 +0200

    fix: restore primaryLight dangerLight (#299)

--- src/apps/renderer/components/Button.tsx
+++ src/apps/renderer/components/Button.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: 'primary' | 'danger' | 'secondary' | 'outline';
+  variant?: 'primary' | 'danger' | 'secondary' | 'outline' | 'primaryLight' | 'dangerLight';
   size?: 'sm' | 'md' | 'lg' | 'xl' | '2xl';
   children: ReactNode;
   customClassName?: string;
@@ -21,12 +21,18 @@ export default function Button({
     primary: buttonProps.disabled
       ? 'bg-gray-30 dark:bg-gray-5 text-white dark:text-gray-30'
       : 'bg-primary active:bg-primary-dark text-white',
+    primaryLight: buttonProps.disabled
+      ? 'bg-gray-30 dark:bg-gray-5 text-white dark:text-gray-30'
+      : 'border border-primary bg-surface text-primary hover:cursor-pointer',
     secondary: buttonProps.disabled
       ? 'bg-surface text-gray-40 border border-gray-5 dark:bg-gray-5 dark:text-gray-40'
       : 'bg-surface active:bg-gray-1 text-highlight border border-gray-20 dark:bg-gray-5 dark:active:bg-gray-10 dark:active:border-gray-30',
     danger: buttonProps.disabled
       ? 'bg-gray-30 dark:bg-gray-5 text-white dark:text-gray-30'
       : 'bg-red active:bg-red-dark text-white',
+    dangerLight: buttonProps.disabled
+      ? 'bg-gray-30 dark:bg-gray-5 text-white dark:text-gray-30'
+      : 'border border-red-dark bg-surface text-red-dark hover:cursor-pointer',
     outline: buttonProps.disabled
       ? 'bg-transparent border-2 border-gray-30 text-gray-30 dark:border-gray-40 dark:text-gray-40 font-bold'
       : 'bg-transparent border-2 border-primary text-primary active:bg-primary/10 dark:border-primary dark:text-primary font-bold',
