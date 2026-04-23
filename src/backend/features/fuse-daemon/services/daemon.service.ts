09b04f20ed34adb3a745d6680fcdb16ec62fbcd1
Alexis Mora <alexis@internxt.com>
2026-04-23 17:16:04 +0200

    Chore: add golang lint + testing pipline + testing http server (#317)
    * feat: golang lint
    
    * feat: golang test pipeline
    
    * chore: go daemon testing
    
    * chore: testing for virtual drive module
    
    * fix: go lint workflow
    
    * fix: go lint workflow bump version
    
    * chore: lint issues in golang
    
    * fix: pr comments
    
    * fix: format

--- src/backend/features/fuse-daemon/services/daemon.service.ts
+++
@@ -1,9 +0,0 @@
-let resolveReady: () => void;
-
-export const daemonReady = new Promise<void>((resolve) => {
-  resolveReady = resolve;
-});
-
-export function resolveDaemonReady(): void {
-  resolveReady();
-}
