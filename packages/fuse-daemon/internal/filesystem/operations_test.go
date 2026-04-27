3c529b8ac71955c8ccd791a719c684be50a88185
AlexisMora <alexis@internxt.com>
2026-04-27 16:51:28 +0200

    chore:tests

--- packages/fuse-daemon/internal/filesystem/operations_test.go
+++ packages/fuse-daemon/internal/filesystem/operations_test.go
@@ -17,14 +17,15 @@ func TestGetAttr(t *testing.T) {
 		now := time.Now().Truncate(time.Second)
 
 		sharedMount.mockServer.setHandler(client.OperationGetAttr, func(response http.ResponseWriter, request *http.Request) {
-			respondJSON(response, GetAttributesCallbackData{
-				Mode:  0o100644,
-				Size:  1234,
-				Mtime: now,
-				Ctime: now,
-				Uid:   uint32(os.Getuid()),
-				Gid:   uint32(os.Getgid()),
-				Nlink: 1,
+			respondJSON(response, map[string]any{
+				"errno": 0,
+				"mode":  0o100644,
+				"size":  1234,
+				"mtime": now,
+				"ctime": now,
+				"uid":   uint32(os.Getuid()),
+				"gid":   uint32(os.Getgid()),
+				"nlink": 1,
 			})
 		})
 
@@ -48,14 +49,15 @@ func TestGetAttr(t *testing.T) {
 
 	t.Run("returns directory attributes", func(t *testing.T) {
 		sharedMount.mockServer.setHandler(client.OperationGetAttr, func(response http.ResponseWriter, request *http.Request) {
-			respondJSON(response, GetAttributesCallbackData{
-				Mode:  0o040755,
-				Size:  4096,
-				Mtime: time.Now(),
-				Ctime: time.Now(),
-				Uid:   uint32(os.Getuid()),
-				Gid:   uint32(os.Getgid()),
-				Nlink: 2,
+			respondJSON(response, map[string]any{
+				"errno": 0,
+				"mode":  0o040755,
+				"size":  4096,
+				"mtime": time.Now(),
+				"ctime": time.Now(),
+				"uid":   uint32(os.Getuid()),
+				"gid":   uint32(os.Getgid()),
+				"nlink": 2,
 			})
 		})
 
@@ -69,7 +71,7 @@ func TestGetAttr(t *testing.T) {
 		}
 	})
 
-	t.Run("returns EIO when the server returns an error", func(t *testing.T) {
+	t.Run("returns EIO on transport failure", func(t *testing.T) {
 		sharedMount.mockServer.setHandler(client.OperationGetAttr, func(response http.ResponseWriter, request *http.Request) {
 			response.WriteHeader(http.StatusInternalServerError)
 		})
@@ -86,6 +88,195 @@ func TestGetAttr(t *testing.T) {
 
 		if pathErr.Err != syscall.EIO {
 			t.Errorf("expected EIO, got %v", pathErr.Err)
+		}
+	})
+
+	t.Run("returns ENOENT when errno is 2", func(t *testing.T) {
+		sharedMount.mockServer.setHandler(client.OperationGetAttr, func(response http.ResponseWriter, request *http.Request) {
+			respondJSON(response, client.ErrorResponse{Errno: 2})
+		})
+
+		_, err := os.Stat(filepath.Join(sharedMount.mountPoint, fmt.Sprintf("missing-%d.txt", time.Now().UnixNano())))
+		if err == nil {
+			t.Fatal("expected error, got nil")
+		}
+
+		pathErr, ok := err.(*os.PathError)
+		if !ok {
+			t.Fatalf("expected *os.PathError, got %T", err)
+		}
+
+		if pathErr.Err != syscall.ENOENT {
+			t.Errorf("expected ENOENT, got %v", pathErr.Err)
+		}
+	})
+}
+
+func dirAttrHandler(response http.ResponseWriter, request *http.Request) {
+	respondJSON(response, map[string]any{
+		"errno": 0,
+		"mode":  0o040755,
+		"size":  0,
+		"mtime": time.Now(),
+		"ctime": time.Now(),
+		"uid":   uint32(os.Getuid()),
+		"gid":   uint32(os.Getgid()),
+		"nlink": 2,
+	})
+}
+
+func fileAttrHandler(response http.ResponseWriter, request *http.Request) {
+	respondJSON(response, map[string]any{
+		"errno": 0,
+		"mode":  0o100644,
+		"size":  0,
+		"mtime": time.Now(),
+		"ctime": time.Now(),
+		"uid":   uint32(os.Getuid()),
+		"gid":   uint32(os.Getgid()),
+		"nlink": 1,
+	})
+}
+
+func TestOpenDir(t *testing.T) {
+	t.Run("returns directory entries", func(t *testing.T) {
+		sharedMount.mockServer.setHandlers(map[client.OperationPath]http.HandlerFunc{
+			client.OperationGetAttr: dirAttrHandler,
+			client.OperationOpenDir: func(response http.ResponseWriter, request *http.Request) {
+				respondJSON(response, map[string]any{
+					"errno": 0,
+					"entries": []map[string]any{
+						{"name": "file.txt", "mode": 0o100644},
+						{"name": "subdir", "mode": 0o040755},
+					},
+				})
+			},
+		})
+
+		entries, err := os.ReadDir(filepath.Join(sharedMount.mountPoint, fmt.Sprintf("mydir-%d", time.Now().UnixNano())))
+		if err != nil {
+			t.Fatalf("readdir: %v", err)
+		}
+
+		if len(entries) != 2 {
+			t.Fatalf("entries: got %d, want 2", len(entries))
+		}
+
+		if entries[0].Name() != "file.txt" {
+			t.Errorf("entry[0]: got %q, want %q", entries[0].Name(), "file.txt")
+		}
+
+		if entries[1].Name() != "subdir" {
+			t.Errorf("entry[1]: got %q, want %q", entries[1].Name(), "subdir")
+		}
+	})
+
+	t.Run("returns EIO on transport failure", func(t *testing.T) {
+		sharedMount.mockServer.setHandlers(map[client.OperationPath]http.HandlerFunc{
+			client.OperationGetAttr: dirAttrHandler,
+			client.OperationOpenDir: func(response http.ResponseWriter, request *http.Request) {
+				response.WriteHeader(http.StatusInternalServerError)
+			},
+		})
+
+		_, err := os.ReadDir(filepath.Join(sharedMount.mountPoint, fmt.Sprintf("broken-%d", time.Now().UnixNano())))
+		if err == nil {
+			t.Fatal("expected error, got nil")
+		}
+
+		pathErr, ok := err.(*os.PathError)
+		if !ok {
+			t.Fatalf("expected *os.PathError, got %T", err)
+		}
+
+		if pathErr.Err != syscall.EIO {
+			t.Errorf("expected EIO, got %v", pathErr.Err)
+		}
+	})
+
+	t.Run("returns ENOENT when errno is 2", func(t *testing.T) {
+		sharedMount.mockServer.setHandlers(map[client.OperationPath]http.HandlerFunc{
+			client.OperationGetAttr: dirAttrHandler,
+			client.OperationOpenDir: func(response http.ResponseWriter, request *http.Request) {
+				respondJSON(response, client.ErrorResponse{Errno: 2})
+			},
+		})
+
+		_, err := os.ReadDir(filepath.Join(sharedMount.mountPoint, fmt.Sprintf("missing-%d", time.Now().UnixNano())))
+		if err == nil {
+			t.Fatal("expected error, got nil")
+		}
+
+		pathErr, ok := err.(*os.PathError)
+		if !ok {
+			t.Fatalf("expected *os.PathError, got %T", err)
+		}
+
+		if pathErr.Err != syscall.ENOENT {
+			t.Errorf("expected ENOENT, got %v", pathErr.Err)
+		}
+	})
+}
+
+func TestOpen(t *testing.T) {
+	t.Run("opens file successfully", func(t *testing.T) {
+		sharedMount.mockServer.setHandlers(map[client.OperationPath]http.HandlerFunc{
+			client.OperationGetAttr: fileAttrHandler,
+			client.OperationOpen: func(response http.ResponseWriter, request *http.Request) {
+				respondJSON(response, client.ErrorResponse{Errno: 0})
+			},
+		})
+
+		f, err := os.Open(filepath.Join(sharedMount.mountPoint, fmt.Sprintf("file-%d.txt", time.Now().UnixNano())))
+		if err != nil {
+			t.Fatalf("open: %v", err)
+		}
+		f.Close()
+	})
+
+	t.Run("returns EIO on transport failure", func(t *testing.T) {
+		sharedMount.mockServer.setHandlers(map[client.OperationPath]http.HandlerFunc{
+			client.OperationGetAttr: fileAttrHandler,
+			client.OperationOpen: func(response http.ResponseWriter, request *http.Request) {
+				response.WriteHeader(http.StatusInternalServerError)
+			},
+		})
+
+		_, err := os.Open(filepath.Join(sharedMount.mountPoint, fmt.Sprintf("broken-%d.txt", time.Now().UnixNano())))
+		if err == nil {
+			t.Fatal("expected error, got nil")
+		}
+
+		pathErr, ok := err.(*os.PathError)
+		if !ok {
+			t.Fatalf("expected *os.PathError, got %T", err)
+		}
+
+		if pathErr.Err != syscall.EIO {
+			t.Errorf("expected EIO, got %v", pathErr.Err)
+		}
+	})
+
+	t.Run("returns ENOENT when errno is 2", func(t *testing.T) {
+		sharedMount.mockServer.setHandlers(map[client.OperationPath]http.HandlerFunc{
+			client.OperationGetAttr: fileAttrHandler,
+			client.OperationOpen: func(response http.ResponseWriter, request *http.Request) {
+				respondJSON(response, client.ErrorResponse{Errno: 2})
+			},
+		})
+
+		_, err := os.Open(filepath.Join(sharedMount.mountPoint, fmt.Sprintf("missing-%d.txt", time.Now().UnixNano())))
+		if err == nil {
+			t.Fatal("expected error, got nil")
+		}
+
+		pathErr, ok := err.(*os.PathError)
+		if !ok {
+			t.Fatalf("expected *os.PathError, got %T", err)
+		}
+
+		if pathErr.Err != syscall.ENOENT {
+			t.Errorf("expected ENOENT, got %v", pathErr.Err)
 		}
 	})
 }
