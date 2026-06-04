d99712eeb5c54100f893aefbb6d6e62ef261c4e7
Esteban Galvis <egalvis@shokworks.io>
2026-06-04 08:43:39 -0500

    chore: remove focusExistingApp function call from second-instance handler

--- src/core/bootstrap/register-second-instance-flow.ts
+++ src/core/bootstrap/register-second-instance-flow.ts
@@ -5,28 +5,11 @@ import { getIsLoggedIn } from '../../apps/main/auth/handlers';
 import { createAuthWindow, getAuthWindow } from '../../apps/main/windows/auth';
 import { getOrCreateWidged } from '../../apps/main/windows/widget';
 
-async function focusExistingApp() {
-  app.focus();
-
-  if (getIsLoggedIn()) {
-    const widget = await getOrCreateWidged();
-    widget?.show();
-    widget?.focus();
-    return;
-  }
-
-  await createAuthWindow();
-  const authWindow = getAuthWindow();
-  authWindow?.show();
-  authWindow?.focus();
-}
-
 export function registerSecondInstanceFlow() {
   app.on('second-instance', async (_, argv) => {
     logger.debug({ tag: 'AUTH', msg: 'Deeplink received on second instance, processing...' });
     const deeplinkArg = argv.find((arg) => arg.startsWith('internxt://'));
     if (!deeplinkArg) {
-      await focusExistingApp();
       return;
     }
 
