c4fcc87be5d77458319bf01960d53a215aecd15a
Esteban Galvis <egalvis@shokworks.io>
2026-07-13 23:38:17 -0500

    chore: Clean up code by adding missing newlines at the end of files

--- src/context/shared/domain/value-objects/BucketEntry.ts
+++ src/context/shared/domain/value-objects/BucketEntry.ts
@@ -1,14 +1,16 @@
-import { ABSOLUTE_UPLOAD_FILE_SIZE_LIMIT } from '../../../../backend/features/user/file-size-limit';
+import { ABSOLUTE_UPLOAD_FILE_SIZE_LIMIT } from '../../../../backend/features/user/file-size-limit/constants';
 import { ValueObject } from './ValueObject';
 
 export class BucketEntry extends ValueObject<number> {
+  static readonly MAX_SIZE = ABSOLUTE_UPLOAD_FILE_SIZE_LIMIT;
+
   constructor(value: number) {
     super(value);
     this.ensureIsValid(value);
   }
 
   private ensureIsValid(value: number) {
-    if (value > ABSOLUTE_UPLOAD_FILE_SIZE_LIMIT) {
+    if (value > BucketEntry.MAX_SIZE) {
       throw new Error('File size to big');
     }
 
