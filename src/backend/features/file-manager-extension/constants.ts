2aea7889b9621d26774fe8b371c2430fc13264c4
Esteban Galvis <egalvis@shokworks.io>
2026-07-30 11:11:03 -0500

    feat: Add documentation for prerequisites and implement smoke tests for file system extension (#421)

---
+++ src/backend/features/file-manager-extension/constants.ts
@@ -0,0 +1,6 @@
+const supportedFileManagers = ['nautilus', 'nemo', 'dolphin'] as const;
+type SupportedFileManager = (typeof supportedFileManagers)[number];
+
+export function isSupportedFileManager(value: string): value is SupportedFileManager {
+  return supportedFileManagers.some((sfm) => sfm === value);
+}
